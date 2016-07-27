4e438dbc5a5a92f14879750e78c8b3cd5cf1d6bb
Aral Roca <aral.roca@greenlightech.com>
2016-07-27 23:54:38 +0200

    Add cross origin in openweathermap API

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -8,7 +8,7 @@ export class WeatherService {
   constructor(private http: Http) { }
 
   getWeather(lat: number, lng: number) {
-    const uri = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=668c90201a44d04c13130251c595f627`;
+    const uri = `https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=668c90201a44d04c13130251c595f627`;
 
     return this.http.get(uri)
       .map(response => response.json());
